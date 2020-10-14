488e245bd265ecdf7a48f7db721772bf9b9f9f92
edson <edsonibarra97@gmail.com>
2020-10-14 00:46:27 -0500

    funciones

--- js/main.js
+++ js/main.js
@@ -1,11 +1,38 @@
-let n = 12
-let c = 20
-let t = 'multiple'
-let d = 'hard'
+// https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple 
 
-fetch(`https://opentdb.com/api.php?amount=${n}&category=${c}&difficulty=${d}&type=${t}`)
+// const q = 30;
+// const c = 9;
+// const d = 'easy';
+// const t = 'multiple';
+
+function numberOfQuestions () {
+    const numberOfQuestions = document.getElementById('n-questions').value
+    return numberOfQuestions
+}
+
+
+function category () {
+    const category = document.getElementById('category-selected').value
+    return category
+}
+
+function difficulty () {
+    const difficulty = document.getElementById('difficulty-selected').value
+    return difficulty
+}
+
+function type () {
+    const type = document.getElementById('type-selected').value
+    return type
+}
+
+
+fetch(`https://opentdb.com/api.php?amount=${numberOfQuestions()}&category=${category()}&difficulty=${difficulty()}&type=${type()}`)
     .then((response) => response.json())
     .then((data) => printCards(data))
+
+
+
     
 function printCards(questions) {
     console.log(questions);
